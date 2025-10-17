cc899b40b09a5a9dd57a15cc70c93496b5494bbb
djdropthebeat-tech <megan0237@gmail.com>
2025-10-17 13:11:18 +0900

    a1017

---
+++ 1017/react/test1/src/App.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import './App.css'
+
+
+function App(){
+  
+  const [count,setCount]=useState(0);
+  return(
+    <div className='container'>
+
+      <h1 className='heading'>리액트카운터예제</h1>
+      <p className='count'>현재 카운트 : {count}</p>
+      <div className='buttons'>
+        <button className='btn' onClick = {()=>setCount(count+1)}> +1증가</button>
+        <button className='btn' onClick = {()=>setCount(count-1)}> +1증가</button>
+                          {/*setCount :count를 변경할 수 있음 */}
+      </div>
+    </div>
+  );
+
+}
+export default App;
