2b7cef7886ade52fac9b6f072bd6b6acca9edcd0
djdropthebeat-tech <megan0237@gmail.com>
2025-10-22 16:13:34 +0900

    a1022

--- react/input/src/User.js
+++ react/input/src/User.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React,{useEffect} from 'react';
 
-export default function User({user}) {
+
+export default function User({user,onRemove,onToggle}) {
+
+  useEffect(()=>{  
+    console.log('user 값이 설정됨');
+    console.log(user);npx 
+    return() => {  
+      console.log('user가 바뀌기전');
+      console.log(user);
+
+    };
+  },[user]);
+
   return (
     <div>
-        <b>{user.username}</b> <span>({user.email})</span>
+        <b style={{
+          cursor:'pointer',
+          color:user.active ? 'green':'black'
+          }}
+          onClick={()=>onToggle(user.id)}
+        > 
+          {user.username}
+        </b>
+        
+        <span>({user.email})</span>
+        <button onClick={()=>onRemove(user.id)}>삭제</button>
     </div>
   );
 }
