cea2ada2dd6571ab87450d25315f3a25775c94d3
https://github.com/djdropthebeat-tech/frontend.git <megan0237@gmail.com>
2025-11-29 20:35:45 +0900

    a1129

--- 00웹디연습/부여가을연꽃축제/제출/js/script1.js
+++ 00웹디연습/부여가을연꽃축제/제출/js/script1.js
@@ -10,4 +10,27 @@ $(function(){
 
 
 
+    var i = 0;
+	
+	setInterval(function(){
+		i++;
+		if(i==3){
+			i=0;
+		}
+		$(".slide").eq(i).stop().fadeIn();
+		$(".slide").eq(i-1).stop().fadeOut();
+		
+		
+	},3000);
+
+
+
+
+    $(".t1").click(function(){
+        $(".pop").stop().fadeIn();
+    });
+    $("button").click(function(){
+        $(".pop").stop().fadeOut();
+    });
+
 });
