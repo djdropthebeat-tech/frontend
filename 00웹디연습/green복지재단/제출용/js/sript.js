c1613497bf09525482960a6573ceb3c951907977
djdropthebeat-tech <megan0237@gmail.com>
2025-12-01 17:25:41 +0900

    a1201

--- 00웹디연습/green복지재단/제출용/js/sript.js
+++ 00웹디연습/green복지재단/제출용/js/sript.js
@@ -10,7 +10,7 @@ $(function(){
    var i = 0;
 
     setInterval(function(){
-        var next = (i + 1) % 3;   // 다음 번호(0→1→2→0)
+        var next = (i + 1) % 3;   
 
         $(".slide").eq(next).css("left","100%").animate({left:0},600);
         $(".slide").eq(i).css("left",0).animate({left:"-100%"},600);
---
+++ 00웹디연습/김치연구소/제출용/js/script.js
@@ -0,0 +1,27 @@
+$(function(){
+
+    $(".menu-title a").mouseenter(function(){
+        $("nav>ul").stop().slideDown()
+        $(this).next.stop().slideUp()
+    });
+    $("nav").mouseleave(function(){
+        $("nav ul").stop().slideUp()
+    });
+
+    $("t1").click(function(){
+        $(".pop").stop().fadeIn();
+    });
+    $("button").click(function(){
+        $(".pop").stop().fadeOut();
+    });
+    
+    var i = 0;
+
+    setInterval(function(){
+        var next = (i + 1) % 3;   
+
+        $(".slide").eq(next).css("left", "100%").animate({left:0}, 600);
+        $(".slide").eq(i).css("left", 0).animate({left:"-100%"}, 600);
+        i = next;
+    },3000);
+});
