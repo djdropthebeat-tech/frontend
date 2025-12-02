762531ed7984d70ae2253102640b3a581639c187
djdropthebeat-tech <megan0237@gmail.com>
2025-12-02 17:59:45 +0900

    a1202

--- 00웹디연습/남도맛기행/js/script.js
+++ 00웹디연습/남도맛기행/js/script.js
@@ -9,21 +9,34 @@ $(function(){
     })
 
 
-var i = 0;
+// var i = 0;
 	
-	setInterval(function(){
-		i++;
-		if(i===3){
-			i=0;
-		}
-		$(".img-slider>.slider>.slide").eq(i).css("left","100%").stop().animate({
-			"left":0
-		});
-		$(".img-slider>.slider>.slide").eq(i-1).css("left","0%").stop().animate({
-			"left":"-100%"
-		});
+// 	setInterval(function(){
+// 		i++;
+// 		if(i===3){
+// 			i=0;
+// 		}
+// 		$(".img-slider>.slider>.slide").eq(i).css("left","100%").stop().animate({
+// 			"left":0
+// 		});
+// 		$(".img-slider>.slider>.slide").eq(i-1).css("left","0%").stop().animate({
+// 			"left":"-100%"
+// 		});
 		
-	},3000);
+// 	},3000);
+
+
+var i = 0;
+
+setInterval(function(){
+	var next =(i+1)%3;
+	
+	$(".slide").eq(next).css("left","100%").animate({left:0},600);
+	$(".slide").eq(i).css("left",0).animate({left:"-100%"},600)
+
+	i = next;
+
+},3000);
 
     $("menu ul li").click(function(){
 		var i = $(this).index();
@@ -31,6 +44,8 @@ var i = 0;
 		$(".sub").css("display","none").eq(i).css("display","block");
 		$(".menu ul li").removeClass("on").eq(i).addClass("on");
 	});
+	
+
 	
 	$(".t1").click(function(){
 		$(".pop").stop().fadeIn();
